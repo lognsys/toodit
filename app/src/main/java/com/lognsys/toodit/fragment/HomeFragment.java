655596d13e48c582f74e3ca89ee65fee7e93eb0e
Priyank <Priyank.Doshi@ucop.edu>
2017-01-24 17:53:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/lognsys/toodit/fragment/HomeFragment.java
@@ -0,0 +1,83 @@
+package com.lognsys.toodit.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.lognsys.toodit.R;
+import com.lognsys.toodit.adapter.ImageAdapter;
+
+
+public class HomeFragment extends Fragment {
+
+    private static final String TAG = "HomeFragment";
+
+    private static final String ARG_ = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+        Log.d(TAG, "HOME FRAGMENT CALLED");
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment HomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View homeFragmentView = inflater.inflate(R.layout.fragment_home, container, false);
+        GridView gridview = (GridView) homeFragmentView.findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(getContext()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getActivity(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return homeFragmentView;
+    }
+
+
+}
---
+++ app/src/main/java/com/lognsys/toodit/fragment/NotificationFragment.java
@@ -0,0 +1,138 @@
+package com.lognsys.toodit.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.lognsys.toodit.R;
+
+
+
+public class NotificationFragment extends Fragment {
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_TEXT = "arg_text";
+    private static final String ARG_COLOR = "arg_color";
+
+
+    private String mText;
+    private int mColor;
+
+    private View mContent;
+    private TextView mTextView;
+   // private OnFragmentInteractionListener mListener;
+
+    public NotificationFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param text Parameter 1.
+     * @param color Parameter 2.
+     * @return A new instance of fragment NotificationFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NotificationFragment newInstance(String text, int color) {
+        NotificationFragment fragment = new NotificationFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TEXT, text);
+        args.putInt(ARG_COLOR, color);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mText = getArguments().getString(ARG_TEXT);
+            mColor = getArguments().getInt(ARG_COLOR);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notification, container, false);
+    }
+
+//    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+//
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // retrieve text and color from bundle or savedInstanceState
+        if (savedInstanceState == null) {
+            Bundle args = getArguments();
+            mText = args.getString(ARG_TEXT);
+            mColor = args.getInt(ARG_COLOR);
+        } else {
+            mText = savedInstanceState.getString(ARG_TEXT);
+            mColor = savedInstanceState.getInt(ARG_COLOR);
+        }
+
+        // initialize views
+        mContent = view.findViewById(R.id.fragment_notify);
+        mTextView = (TextView) view.findViewById(R.id.text);
+
+        // set text and background color
+        mTextView.setText(mText);
+        mContent.setBackgroundColor(mColor);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(ARG_TEXT, mText);
+        outState.putInt(ARG_COLOR, mColor);
+        super.onSaveInstanceState(outState);
+    }
+}
