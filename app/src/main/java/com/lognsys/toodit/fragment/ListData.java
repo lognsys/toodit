ba5efa457e08ad1ede55eaf7bfd51ec33d4ef651
Akhilesh Yadav <akhilesh.yadav@lognsystems.com>
2017-02-16 18:10:57 +0530

    changes in notification, cart and centralGrillFrag

---
+++ app/src/main/java/com/lognsys/toodit/fragment/ListData.java
@@ -0,0 +1,33 @@
+package com.lognsys.toodit.fragment;
+
+/**
+ * Created by admin on 06-02-2017.
+ */
+
+
+/*
+ * ListData class will hold data for displaying in ListView
+ * */
+public class ListData {
+
+    String comment;
+    String profileName;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+
+}
